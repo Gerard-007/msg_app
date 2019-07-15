94b47dd55ed5f262f72ed0b0600e67bb5070f954
Gerard-007 <gerardnwazk@gmail.com>
2019-07-15 12:30:39 +0000

    Just made my first commit.

--- msg/accounts/urls.py
+++ msg/accounts/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	url(r"login/$", views.LoginView.as_view(), name="login"),
 ]
--- msg/accounts/views.py
+++ msg/accounts/views.py
@@ -5,4 +5,15 @@ from django.views.generic import FormView
 # Create your views here.
 
 class LoginView(FormView):
-	form_class
+	form_class = AuthenticationForm
+	success_url = reverse_lazy("posts:all")
+	template_name = "accounts/login.html"
+	
+	def get_form(self, form_class=None):
+		if form_class is None:
+			form_class = self.get_form_class()
+		return form_class(self.request, **self.get_form_kwargs())
+	
+	def form_valid(self, form):
+		login(self.request, form.get_user())
+		return super().form_valid(form)
